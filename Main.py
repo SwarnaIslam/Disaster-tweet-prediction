c5a7eb2e1013fe8de067181c0ecec12518b1dbc9
SwarnaIslam <bsse1214@iit.du.ac.bd>
2023-10-27 21:58:44 +0600

    train and test dataset splited

---
+++ Main.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import itertools
+import seaborn as sns
+import nltk, re, string
+from string import punctuation
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, precision_score, confusion_matrix, recall_score, roc_auc_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import PassiveAggressiveClassifier, LogisticRegression
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from wordcloud import WordCloud
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+df = pd.read_csv('train.csv')
+
+stop = set(stopwords.words('english'))
+lemma = WordNetLemmatizer()
+
+
+def cleanTweet(txt):
+    txt = txt.lower()
+    words = nltk.word_tokenize(txt)
+    words = ' '.join([lemma.lemmatize(word) for word in words if word not in (stop)])
+    text = "".join(words)
+    txt = re.sub('[^a-z]', ' ', text)
+    return txt
+
+
+df['cleaned_tweets'] = df['text'].apply(cleanTweet)
+
+y=df.target
+x=df.cleaned_tweets
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.20,stratify=y, random_state=0)
+
+
+plt.show()
