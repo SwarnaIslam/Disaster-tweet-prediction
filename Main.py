fe1b9fb153222bb13c084c910a8488a8a5b6402a
SwarnaIslam <bsse1214@iit.du.ac.bd>
2023-10-29 02:34:01 +0600

    term freqency and inverse docoment frequency done

--- Main.py
+++ Main.py
@@ -1,8 +1,17 @@
 import pandas as pd
+import numpy as np
+import itertools
+import seaborn as sns
 import nltk, re, string
+from string import punctuation
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, precision_score, confusion_matrix, recall_score, roc_auc_score
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import PassiveAggressiveClassifier, LogisticRegression
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from wordcloud import WordCloud
 from nltk.stem import WordNetLemmatizer
 
 nltk.download('punkt')
@@ -27,9 +36,11 @@ def cleanTweet(txt):
 
 df['cleaned_tweets'] = df['text'].apply(cleanTweet)
 
-y=df.target
-x=df.cleaned_tweets
-x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.20,stratify=y, random_state=0)
+y = df.target
+x = df.cleaned_tweets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, stratify=y, random_state=0)
 
-
+tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.8, ngram_range=(1, 2))
+tfidf_train_2 = tfidf_vectorizer.fit_transform(x_train)
+tfidf_test_2 = tfidf_vectorizer.transform(x_test)
 plt.show()
