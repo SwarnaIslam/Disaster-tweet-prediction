8650aba063826037a34d02d37917680ecb6b36b7
SwarnaIslam <bsse1214@iit.du.ac.bd>
2023-11-07 16:54:21 +0600

    Predicting if a tweet is disaster tweet or not

--- Main.py
+++ Main.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import PassiveAggressiveClassifier, LogisticRegression
 from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from wordcloud import WordCloud
+from sklearn import model_selection
 from nltk.stem import WordNetLemmatizer
 
 nltk.download('punkt')
@@ -38,17 +39,23 @@ df['cleaned_tweets'] = df['text'].apply(cleanTweet)
 
 y = df.target
 x = df.cleaned_tweets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, stratify=y, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, stratify=y, random_state=0)
 
-tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.8, ngram_range=(1, 2))
-tfidf_train_2 = tfidf_vectorizer.fit_transform(x_train)
-tfidf_test_2 = tfidf_vectorizer.transform(x_test)
+tfidf_vectorizer_3 = TfidfVectorizer(stop_words='english', max_df=0.8, ngram_range=(1,3))
+tfidf_train_3 = tfidf_vectorizer_3.fit_transform(x_train)
+tfidf_test_3 = tfidf_vectorizer_3.transform(x_test)
 
-mnb_tf = MultinomialNB()
-mnb_tf.fit(tfidf_train_2, y_train)
+pass_tf3 = PassiveAggressiveClassifier()
+pass_tf3.fit(tfidf_train_3, y_train)
 
-pred_mnb2 = mnb_tf.predict(tfidf_test_2)
-CM=confusion_matrix(y_test,pred_mnb2)
+kfold = model_selection.KFold(n_splits=10)
+scoring = 'accuracy'
+
+acc_pass3 = cross_val_score(estimator = pass_tf3, X = tfidf_train_3, y = y_train, cv = kfold,scoring=scoring)
+acc_pass3.mean()
+
+pred_pass3 = pass_tf3.predict(tfidf_test_3)
+CM=confusion_matrix(y_test,pred_pass3)
 
 TN = CM[0][0]
 FN = CM[1][0]
@@ -56,15 +63,36 @@ TP = CM[1][1]
 FP = CM[0][1]
 specificity = TN/(TN+FP)
 
-acc= accuracy_score(y_test, pred_mnb2)
+acc = accuracy_score(y_test, pred_pass3)
+prec = precision_score(y_test, pred_pass3)
+rec = recall_score(y_test, pred_pass3)
+f1 = f1_score(y_test, pred_pass3)
 
-prec = precision_score(y_test, pred_mnb2)
-rec = recall_score(y_test, pred_mnb2)
-f1 = f1_score(y_test, pred_mnb2)
-
-
-model_results =pd.DataFrame([['Multinomial Naive Bayes - TFIDF-Bigram',acc, prec,rec,specificity, f1]],
+mod1_results =pd.DataFrame([['Passive Aggressive Classifier - TFIDF-Trigram',acc, prec,rec,specificity, f1]],
                columns = ['Model', 'Accuracy','Precision', 'Sensitivity','Specificity', 'F1 Score'])
+print(mod1_results)
 
-print(model_results)
+sentences = [
+  "Just happened a terrible car crash",
+    "Heard about #earthquake is different cities, stay safe everyone.",
+    "No I don't like cold!",
+    "@RosieGray Now in all sincerety do you think the UN would move to Israel if there was a fraction of a chance of being annihilated?",
+    "Hiroshima was a nuclear bomb disaster",
+    "My heart is shaking with sorrow"
+]
+
+tfidf_trigram = tfidf_vectorizer_3.transform(sentences)
+
+
+predictions = pass_tf3.predict(tfidf_trigram)
+
+for text, label in zip(sentences, predictions):
+    if label==1:
+        target="Disaster Tweet"
+        print("text:", text, "\nClass:", target)
+        print()
+    else:
+        target="Normal Tweet"
+        print("text:", text, "\nClass:", target)
+        print()
 plt.show()
